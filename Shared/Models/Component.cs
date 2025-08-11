b8ded49fe7bcfc1a6ffb72ce2be9362c87cd6e06
TonIO <pancla13ulas@gmail.com>
2025-08-11 17:28:42 +0800

    Subject Template clean up

--- Shared/Models/Component.cs
+++ Shared/Models/Component.cs
@@ -5,6 +5,6 @@ public class Component
     public Guid Id { get; set; } = Guid.NewGuid();
     public Guid SubjectId { get; set; }
     public string Name { get; set; } = string.Empty;
-    public double Percentage { get; set; }
+    public double Percentage { get; set; }  
     public List<Subcomponent> Subcomponents { get; set; } = new();
 }
