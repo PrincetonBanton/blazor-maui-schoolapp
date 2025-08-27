b549f2fc0bafb227543f391d3b3748c3c5c79bec
TonIO <pancla13ulas@gmail.com>
2025-08-27 11:27:20 +0800

    Attendance Calendar Added for Listing

--- Server/Controllers/StudentController.cs
+++ Server/Controllers/StudentController.cs
@@ -43,6 +43,13 @@ public class StudentController : ControllerBase
         return Ok(student);
     }
 
+    [HttpPost]
+    public async Task<ActionResult<Student>> PostStudent(Student student)
+    {
+        _context.Students.Add(student);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
+    }
 
     // PUT: api/Student/{id}
     [HttpPut("{id}")]
