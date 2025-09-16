d792822d8b8b6644baf18d3a4a0f0ab6fb4749b5
TonIO <pancla13ulas@gmail.com>
2025-09-16 14:28:47 +0800

    Shared Context Services test applied on Student and Subject page

--- Shared/Services/SchoolContextService.cs
+++ Shared/Services/SchoolContextService.cs
@@ -1,9 +1,37 @@
-﻿namespace SchoolApp.Shared.Services
+﻿using SchoolApp.Shared.Models;
+
+namespace SchoolApp.Shared.Services
 {
     public class SchoolContextService
     {
         public Guid? SelectedSchoolYearId { get; set; }
         public Guid? SelectedGradeLevelId { get; set; }
         public Guid? SelectedSchoolSectionId { get; set; }
+
+        private List<SchoolYear> SchoolYears { get; set; } = new();
+        private List<GradeLevel> GradeLevels { get; set; } = new();
+        private List<SchoolSection> SchoolSections { get; set; } = new();
+
+
+        public void SetSchoolYears(List<SchoolYear> years) => SchoolYears = years;
+        public void SetGradeLevels(List<GradeLevel> levels) => GradeLevels = levels;
+        public void SetSchoolSections(List<SchoolSection> sections) => SchoolSections = sections;
+        public string GetSelectedSchoolYearName()
+        {
+            var sy = SchoolYears.FirstOrDefault(y => y.Id == SelectedSchoolYearId);
+            return sy?.SchoolYearName ?? "No school year selected";
+        }
+
+        public string GetSelectedGradeLevelName()
+        {
+            var gl = GradeLevels.FirstOrDefault(s => s.Id == SelectedGradeLevelId);
+            return gl?.LevelName ?? "No grade level selected";
+        }
+
+        public string GetSelectedSchoolSectionName()
+        {
+            var ss = SchoolSections.FirstOrDefault(s => s.Id == SelectedSchoolSectionId);
+            return ss?.SectionName ?? "No grade level selected";
+        }
     }
 }
