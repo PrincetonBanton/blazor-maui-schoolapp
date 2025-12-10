a9c5931be1bc1d0c6b9fd4d06e5b58552daa6b55
TonIO <pancla13ulas@gmail.com>
2025-12-10 17:19:15 +0800

    Universal Simple FadeIn Transition Effect

---
+++ Client/Helpers/StudentSeeder.cs
@@ -0,0 +1,47 @@
+﻿using SchoolApp.Shared.Models;
+
+namespace SchoolApp.Client.Helpers
+{
+    public static class StudentSeeder
+    {
+        private static readonly Random rand = new();
+
+        private static readonly string[] firstNames =
+        {
+            "Juan","Pedro","Maria","Jose","Ana","Liza","Carlos","Rosa","Mark","Ella",
+            "John","Paul","Grace","Angel","Leo","Nina","Kevin","Samantha","Daniel","Mia",
+            "Allan","Jessa","Noel","Cathy","Ramon","April","Dennis","Jenny","Victor","Lourdes"
+        };
+
+        private static readonly string[] lastNames =
+        {
+            "Santos","Reyes","Cruz","Dela Cruz","Garcia","Torres","Mendoza","Flores","Lopez","Ramos",
+            "Gonzales","Rivera","Bautista","Castro","Domingo","Fernandez","Gutierrez","Jimenez","Lim","Manalo",
+            "Navarro","Ocampo","Perez","Quinto","Soriano","Tan","Valdez","Yap","Zamora","Villanueva"
+        };
+
+        private static readonly string[] addresses =
+        {
+            "Davao City","Sta. Cruz","Toril","Buhangin","Calinan",
+            "Mintal","Matina","Tugbok","Bago Aplaya","Panacan"
+        };
+
+        private static readonly string[] genders = { "Male", "Female" };
+
+        public static Student GenerateRandomStudent(Guid? gradeLevelId, Guid? sectionId)
+        {
+            return new Student
+            {
+                GradeLevelId = gradeLevelId,
+                SchoolSectionId = sectionId,
+                FirstName = firstNames[rand.Next(firstNames.Length)],
+                MiddleName = "M.",
+                LastName = lastNames[rand.Next(lastNames.Length)],
+                DateOfBirth = DateTime.Now.AddYears(-7 - rand.Next(2)).AddDays(rand.Next(365)),
+                Gender = genders[rand.Next(genders.Length)],
+                Address = addresses[rand.Next(addresses.Length)],
+                ContactNumber = "09" + rand.Next(100000000, 999999999)
+            };
+        }
+    }
+}
---
+++ Client/Helpers/StudentValidator.cs
@@ -0,0 +1,39 @@
+﻿using SchoolApp.Shared.Models;
+
+namespace SchoolApp.Client.Helpers
+{
+    public static class StudentValidator
+    {
+        public static string? Validate(Student student)
+        {
+            if (student == null)
+                return "Invalid student data.";
+
+            if (string.IsNullOrWhiteSpace(student.FirstName))
+                return "First name is required.";
+
+            if (string.IsNullOrWhiteSpace(student.LastName))
+                return "Last name is required.";
+
+            if (student.DateOfBirth == default)
+                return "Date of birth is required.";
+
+            if (string.IsNullOrWhiteSpace(student.Gender))
+                return "Gender is required.";
+
+            if (string.IsNullOrWhiteSpace(student.Address))
+                return "Address is required.";
+
+            if (string.IsNullOrWhiteSpace(student.ContactNumber))
+                return "Contact number is required.";
+
+            if (student.GradeLevelId == null)
+                return "A grade level must be selected.";
+
+            if (student.SchoolSectionId == null)
+                return "A school section must be selected.";
+
+            return null; // no error
+        }
+    }
+}
